30881b2712d68992eb1f8f59fd3016bf5e32273c
hiiamfrankie <daze.frankie@gmail.com>
2016-04-03 22:51:26 +0600

    initial commit

---
+++ app/src/main/java/com/klt/klt/AppCompatActivityExtensions.kt
@@ -0,0 +1,12 @@
+package com.klt.klt
+
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+/**
+ * Created by frankie on 03.04.2016.
+ */
+
+fun AppCompatActivity.toast(text: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, text, duration)
+}
